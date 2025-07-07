6eaf86a1f9c21921b7fae072f62eb4041067f192
どどど素人 <87510462+dododoshirouto@users.noreply.github.com>
2025-07-07 15:15:36 +0900

    Update _install.bat for node modules

--- _install.bat
+++ _install.bat
@@ -6,7 +6,7 @@ echo [!] Require Python 3.10
 
 echo === Make Node.js for root ===
 
-IF EXIST package.json GOTO :root_package.json
+: IF EXIST package.json GOTO :root_package.json
     call npm init -y
     call npm install axios node-wav
 :root_package.json
@@ -19,7 +19,7 @@ IF EXIST read_bookmark GOTO :mkdir_read_bookmark
     mkdir read_bookmark
 :mkdir_read_bookmark
 cd read_bookmark
-IF EXIST package.json GOTO :bookmark_package.json
+: IF EXIST package.json GOTO :bookmark_package.json
     call npm init -y
     call npm install puppeteer child_process
 :bookmark_package.json
@@ -33,7 +33,7 @@ IF EXIST use_chatgpt GOTO :mkdir_use_chatgpt
     mkdir use_chatgpt
 :mkdir_use_chatgpt
 cd use_chatgpt
-IF EXIST package.json GOTO :chatgpt_package.json
+: IF EXIST package.json GOTO :chatgpt_package.json
     call npm init -y
     call npm install openai dotenv
 :chatgpt_package.json
@@ -47,7 +47,7 @@ IF EXIST use_youtube GOTO :mkdir_use_youtube
     mkdir use_youtube
 :mkdir_use_youtube
 cd use_youtube
-IF EXIST package.json GOTO :youtube_package.json
+: IF EXIST package.json GOTO :youtube_package.json
     call npm init -y
     call npm install googleapis open
 :youtube_package.json
